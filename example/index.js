d5634c22d4bd2658cd4fce55431ae335923f2468
Lsong <song940@gmail.com>
2016-03-15 23:51:48 +0800

    modify .

--- example/index.js
+++ example/index.js
@@ -7,14 +7,8 @@ const app = kelp();
 app.use(body);
 
 app.use(function(req, res, next){
-  console.log('----------------------------');
-  console.log('url\r\t\t='    , req.url);
-  console.log('path\r\t\t='   , req.path);
-  console.log('query\r\t\t='  , req.query);
-  console.log('text\r\t\t='   , req.text);
-  console.log('body\r\t\t='   , req.body);
-  console.log('----------------------------');
-  res.end('kelp-body');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<form method="post"><input name="user" type="text" /><input type="submit" /></form>');
 });
 
 const server = http.createServer(app).listen(4000);
--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 'use strict';
-const url = require('url');
-const qs  = require('querystring');
+const url  = require('url');
+const mime = require('./mime');
 
 module.exports = function(req, res, next){
   var u = url.parse(req.url, true);
   req.path  = u.pathname;
   req.query = u.query;
-  
-  var buffer = [];
+
+  req.text = '';
+  req.body = mime(req).body;
   req.on('data', function(chunk){
-    buffer.push(chunk);
+    req.text += chunk;
   })
   .on('end', function(){
-    req.data = buffer;
-    req.text = buffer.join('');
-    switch((req.headers['content-type'] || '').split(';')[0]){
-      case 'application/x-www-form-urlencoded':
-        req.body = qs.parse(req.text);
-        break;
-      case 'application/json':
-        req.body = JSON.parse(req.text);
-        break;
-      case 'multipart/form-data':
-        console.log(req.text);
-      case 'text/plain':
-      default:
-        break;
-    }
+    req.data = new Buffer(req.text);
     next();
   });
 };
