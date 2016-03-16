0174ddec8ef72275ab8768f97c16b0b6cfc27ad3
Lsong <song940@gmail.com>
2016-03-16 23:49:36 +0800

    modify .

--- example/index.js
+++ example/index.js
@@ -5,10 +5,18 @@ const body = require('../');
 const app = kelp();
 
 app.use(body);
-
 app.use(function(req, res, next){
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<form method="post"><input name="user" type="text" /><input type="submit" /></form>');
+  console.log(req.body);
+  console.log(req.files);
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
 });
 
 const server = http.createServer(app).listen(4000);
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 'use strict';
-const url  = require('url');
-const mime = require('./mime');
-
+const url        = require('url');
+const multiparty = require('multiparty');
 module.exports = function(req, res, next){
   var u = url.parse(req.url, true);
   req.path  = u.pathname;
   req.query = u.query;
 
   req.text = '';
-  req.body = mime(req).body;
-  req.on('data', function(chunk){
-    req.text += chunk;
-  })
-  .on('end', function(){
-    req.data = new Buffer(req.text);
+  // parse a file upload
+  var form = new multiparty.Form();
+  form.parse(req, function(err, fields, files) {
+    req.body = fields;
+    req.files = files;
     next();
   });
 };
